99204c4f485bd4d1e01373dbd674dab77d808d8a
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-27 11:50:10 +0300

    NodeJS: Globals

---
+++ 2-globals.js
@@ -0,0 +1,19 @@
+// GLOBALS - NO WINDOW !!!
+
+// __direname - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+
+console.log(__dirname);
+// console.log( __filename);
+// console.log(require);
+
+setInterval(()=>{
+	console.log('hello world')
+}, 1000);
+
+//To stop ctrl+C
+//To clear put in terminal "clear"
+//To test an app "node [name].js"
