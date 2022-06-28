560d289c2dd114947d9496deb0c5cf65333dbd98
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-28 13:20:55 +0300

    NodeJS: Uninstall package

--- 13-npm-commands.js
+++ 13-npm-commands.js
@@ -18,3 +18,6 @@ const _ = require('lodash');
 const items = [1,[2,[3,[4]]]];
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+
+//To uninstall pachage we use 
+// npm uninstall <packageName>
