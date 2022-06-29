883249f0e2dbd450ea5ce3daaf8248b4413dabf5
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-29 13:17:34 +0300

    NodeJS: Async Patterns - Setup Promises

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-const http = require('http');
+const { readFile} = require('fs');
+const { result } = require('lodash');
 
-const server = http.createServer((req,res) => {
-	if(req.url ==='/'){
-		res.end('Home Page');
-	} else if(req.url ==='/about'){
-		// BLOCKING CODE
-		for(let i = 0; i<1000; i++){
-			for(let j = 0; j<1000; j++){
-				console.log(`${i} ${j}`);
+const getText = (path) =>{
+	return new Promise((resolve,reject)=>{
+		readFile(path, 'utf-8', (err,data)=>{
+			if(err) {
+				reject(err);
 			}
-		}
-		res.end('About Page');
-	} else {
-	res.end('Error Page');
-	}
-});
+			else{
+				resolve(data);
+			}
+		});
+	})
+}
 
-server.listen(5000, ()=>{
-	console.log('Server is Listeming on port 5000...');
-});
+getText('./content/first.txt')
+	.then(result => console.log(result))
+	.catch(err=> console.log(err))
