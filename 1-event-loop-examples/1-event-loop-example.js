e9f5cb9e1078b8914e0b0ef4f4ec61cbf6c48efc
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-29 10:58:06 +0300

    NodeJS: Event loop example #1

---
+++ 1-event-loop-examples/1-event-loop-example.js
@@ -0,0 +1,15 @@
+const { readFile } = require('fs')
+
+console.log('started a first task');
+//CHECK FILE PATH!
+
+//readFile is Syncronous, so it runs last
+readFile('./content/first.txt', 'utf-8', (err,result) => {
+	if(err) {
+		console.log(err);
+		return;
+	}
+	console.log(result)
+	console.log('completed first task')
+})
+console.log('starting next task')
