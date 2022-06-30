870f92c53fb41c078fde34ac108af5a91c04f968
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-30 13:25:48 +0300

    Http Basics

--- 2-express-tutorial-Ivan/app.js
+++ 2-express-tutorial-Ivan/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+	res.end('Welcome to our home page');
+	console.log('user hit the server');
+});
+
+server.listen(5000);
