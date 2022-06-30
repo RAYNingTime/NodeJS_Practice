c61bc389c932bccfcc770ac11e550432a17ba3a2
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-30 10:29:20 +0300

    NodeJS: Events Emitter - Code Example

---
+++ 14-event-emitter.js
@@ -0,0 +1,22 @@
+// on and emit methods
+// keep track of the order
+// additional arguments
+// buld-in modules utilize it
+
+const EventEmitter = require('events');
+
+const customEmitter = new EventEmitter();
+
+// on - listen for an event
+// emit - emit(Генерировать) an event
+customEmitter.on('response', (name, id) => {
+	console.log(` data recieve ${name} with id:${id} `);
+});
+
+customEmitter.on('response', () => {
+	console.log(` some other logic here `);
+});
+
+// '.emit' MUST be AFTER '.on'
+customEmitter.emit('response','john',34);
+// ---------------   /\ Call --- /\ Arguments
