6c2abb180c09b7ab431c7ddaa24c8262038e0f68
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-30 14:02:14 +0300

    ExpressJS: Http - Headers

--- 2-express-tutorial-Ivan/app.js
+++ 2-express-tutorial-Ivan/app.js
@@ -1,8 +1,15 @@
+// Status Codes from responses
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+
+// MIME types
+// https://developer.mozilla.org/ru/docs/Web/HTTP/Basics_of_HTTP/MIME_types
+
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-	res.end('Welcome to our home page');
-	console.log('user hit the server');
+	res.writeHead(200,{'content-type':'text/html'});
+	res.write('<h1>Home Page</h1>');
+	res.end();
 });
 
 server.listen(5000);
