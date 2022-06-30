16d62da1244b89fd21746d607c6e4290a647a68a
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-30 11:23:50 +0300

    NodeJS: Streams - Http Example

---
+++ app.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+
+http
+	.createServer(function (req,res) {
+		// const text = fs.readFileSync('./content/big.txt', 'utf8');
+		// res.end(text);
+
+		//In the first method we are sending data as one big file
+		//While in the second we are sending data by chanks
+
+		const fileStream = fs.createReadStream('./content/big.txt','utf8');
+		fileStream.on('open', () => {
+			fileStream.pipe(res);
+		})
+		fileStream.on('error', (err) => {
+			res.end(err);
+		})
+	})
+	.listen(5000);
