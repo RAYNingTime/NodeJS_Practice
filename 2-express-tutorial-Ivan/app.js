3ee73d6c9ae9af8893e98ac7db098bf9d7297420
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-30 14:51:35 +0300

    ExpressJS: Http - App Example

--- 2-express-tutorial-Ivan/app.js
+++ 2-express-tutorial-Ivan/app.js
@@ -8,25 +8,41 @@ const http = require('http');
 const {readFileSync} = require('fs');
 
 // get all files
-const homePage = readFileSync('./index.html');
+const homePage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
 const server = http.createServer((req,res) => {
 	// console.log(req.method);
 	const url = req.url;
 
-	//home page
+	// home page
 	if(url ==='/') {
 	res.writeHead(200,{'content-type':'text/html'});
 	res.write(homePage);
 	res.end();
-	} else
-	//about page
-	if(url ==='/about'){
-		res.writeHead(200,{'content-type':'text/html'});
-		res.write('<h1>About Page</h1>');
+	} 
+	// styles
+	else if(url ==='/styles.css'){
+		res.writeHead(200,{'content-type':'text/css'});
+		res.write(homeStyles);
 		res.end();
+	}
+	// image/logo
+	else if(url ==='/logo.svg'){
+		res.writeHead(200,{'content-type':'image/svg+xml'});
+		res.write(homeImage);
+		res.end();
+	}
+	//logic
+	else if(url ==='/browser-app.js'){
+		res.writeHead(200,{'content-type':'text/javascript'});
+		res.write(homeLogic);
+		res.end();
+	}
 	//404
-	} else {
+	else {
 		res.writeHead(404,{'content-type':'text/html'});
 		res.write('<h1>404. Page not found</h1>');
 		res.end();
