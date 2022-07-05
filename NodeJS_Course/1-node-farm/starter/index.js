737ae0a35628947183ed7e7408daec89ef40c745
RAYNingTime <ivan0kosyakov@gmail.com>
2022-07-05 11:40:18 +0300

    8. Reading and Writing Files

--- NodeJS_Course/1-node-farm/starter/index.js
+++ NodeJS_Course/1-node-farm/starter/index.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
 
-const hello = "Hello world";
-console.log(hello);
+const textFromFile = fs.readFileSync('./txt/input.txt','utf-8');
+
+const textOut = `Text from other file is: \n${textFromFile}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt',textOut);
+console.log('File written!');
