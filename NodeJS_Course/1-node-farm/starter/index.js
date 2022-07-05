abe6ca24b9b31db327b819fcf9e781a0ee1e5afb
RAYNingTime <ivan0kosyakov@gmail.com>
2022-07-05 12:38:09 +0300

    10. Reading and Writing Files Asynchronously

--- NodeJS_Course/1-node-farm/starter/index.js
+++ NodeJS_Course/1-node-farm/starter/index.js
@@ -1,7 +1,25 @@
 const fs = require('fs');
 
-const textFromFile = fs.readFileSync('./txt/input.txt','utf-8');
+// Blocking, synchronous way
+// const textFromFile = fs.readFileSync('./txt/input.txt','utf-8');
+// const textOut = `Text from other file is: \n${textFromFile}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt',textOut);
+// console.log('File written!');
 
-const textOut = `Text from other file is: \n${textFromFile}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt',textOut);
-console.log('File written!');
+// Non-Blocking, asynchronous way
+fs.readFile('./txt/start.txt','utf-8', (err,data1) => {
+	if (err) return console.log(`ERROR.`);
+	fs.readFile(`./txt/${data1}.txt`,'utf-8', (err,data2) => {
+		console.log(data2);
+		fs.readFile('./txt/append.txt','utf-8', (err,data3) => {
+			console.log(data3);
+			
+			fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,(err)=> {
+				//This triangle called "Callback hell"
+				console.log('Your File has been written!');
+			});
+		});
+	});
+});
+
+console.log(`I'm not really sure that it is going to work...`);
