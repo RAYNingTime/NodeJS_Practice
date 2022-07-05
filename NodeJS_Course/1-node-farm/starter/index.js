3dc1ce18fdd65562d272c540a3195b79890d39f4
RAYNingTime <ivan0kosyakov@gmail.com>
2022-07-05 13:57:14 +0300

    12. Routing

--- NodeJS_Course/1-node-farm/starter/index.js
+++ NodeJS_Course/1-node-farm/starter/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const http = require('http');
+const path = require('path');
+const url = require('url');
 
 /////////////////////////////////
 // FILES
@@ -31,7 +33,18 @@ const http = require('http');
 // SERVER
 
 const server = http.createServer((req,res) => {
-	res.end('Hello from the server!');
+	const pathName = req.url;
+	if(pathName === '/' || pathName === '/overview'){
+		res.end('This is the OVERVIEW');
+	} else if (pathName === '/product'){
+		res.end('This is the PRODUCT');
+	} else {
+		res.writeHead(404, {
+			"Content-type": 'text/html',
+			'my-own-header': 'hello-world'
+		});
+		res.end('<h1>ERROR 404</h1>');
+	}
 });
 
 server.listen(8000, '127.0.0.1', () => {
