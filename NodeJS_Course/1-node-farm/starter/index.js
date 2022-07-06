5f2c6287bb673adb0d1c5f0247c300fd2be6e494
RAYNingTime <ivan0kosyakov@gmail.com>
2022-07-06 14:10:43 +0300

    17. Using Modules 2: Our Own Modules

--- NodeJS_Course/1-node-farm/starter/index.js
+++ NodeJS_Course/1-node-farm/starter/index.js
@@ -1,8 +1,8 @@
-const { profileEnd } = require('console');
 const fs = require('fs');
 const http = require('http');
-const path = require('path');
 const url = require('url');
+
+const replaceTemplate = require('./modules/replaceTemplate.js');
 
 /////////////////////////////////
 // FILES
@@ -33,20 +33,6 @@ const url = require('url');
 /////////////////////////////////
 // SERVER
 
-const replaceTemplate = (temp, product) => {
-	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-	output = output.replace(/{%IMAGE%}/g, product.image);
-	output = output.replace(/{%QUANTITY%}/g, product.quantity);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrient);
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-	output = output.replace(/{%ID%}/g, product.id);
-	
-	if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-
-	return output;
-}
 
 //Created once
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
