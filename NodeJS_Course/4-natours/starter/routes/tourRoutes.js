88db13bd0caee40bde946d8fe2235c7bc0fa86bb
RAYNingTime <ivan0kosyakov@gmail.com>
2022-07-13 13:58:59 +0300

    65. Chaining Multiple Middleware Functions(part 1)
    My version from challenge

--- NodeJS_Course/4-natours/starter/routes/tourRoutes.js
+++ NodeJS_Course/4-natours/starter/routes/tourRoutes.js
@@ -5,10 +5,28 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
+// Create a checkBody middleware
+// Check if body contains the name and price property
+// If not, send back 400 (bad request)
+// Add it to the post handler stack
+
+// Assume function is called middleware
+// .post(middleware, tourController.createTour);
+
+checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'This request do not contain Name or Price propery!',
+    });
+  }
+  next();
+};
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(checkBody, tourController.createTour);
 
 router
   .route('/:id')
