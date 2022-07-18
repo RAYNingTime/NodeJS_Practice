5e652958a4752b43700dab005176df812b222b24
RAYNingTime <ivan0kosyakov@gmail.com>
2022-07-18 10:43:17 +0300

    86. Creating Documents and Testing the Model

--- NodeJS_Course/4-natours/starter/server.js
+++ NodeJS_Course/4-natours/starter/server.js
@@ -23,11 +23,11 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique: true
+    unique: true,
   },
   rating: {
     type: Number,
-    default: 4.5
+    default: 4.5,
   },
   price: {
     type: Number,
@@ -36,6 +36,20 @@ const tourSchema = new mongoose.Schema({
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR :', err);
+  });
 
 // console.log(process.env);
 
