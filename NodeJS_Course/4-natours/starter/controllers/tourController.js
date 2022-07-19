d9f709d10ec2c0b5c0a24bb4b7175f2a5f90529c
RAYNingTime <ivan0kosyakov@gmail.com>
2022-07-19 14:37:12 +0300

    96. Making the API Better: Advanced Filtering

--- NodeJS_Course/4-natours/starter/controllers/tourController.js
+++ NodeJS_Course/4-natours/starter/controllers/tourController.js
@@ -2,23 +2,35 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
+
     //BUILD QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    console.log(req.query, queryObj);
+    // 2) Advanced filtering
 
-    const query = Tour.find(queryObj);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    // { difficulty: 'easy', duration: { $gte: 5 }}
+    // { duration: { gte: '5' }, difficulty: 'easy' }
+
+    // gte, gt, lte, lt
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    //EXECUTE QUERY
+    const tours = await query;
 
     // const tours = await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    //EXECUTE QUERY
-    const tours = await query;
 
     //SEND RESPONSE
     res.status(200).json({
