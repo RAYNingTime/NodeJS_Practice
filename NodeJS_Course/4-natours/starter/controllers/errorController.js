945de136d555fc01a1c72e896ea8a62ad974b880
RAYNingTime <ivan0kosyakov@gmail.com>
2022-07-26 11:22:21 +0300

    119. Handling Invalid Database IDs

--- NodeJS_Course/4-natours/starter/controllers/errorController.js
+++ NodeJS_Course/4-natours/starter/controllers/errorController.js
@@ -1,40 +1,41 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    error: err,
     message: err.message,
     stack: err.stack,
+    err,
   });
 };
-
 const sendErrorProd = (err, res) => {
-  // Operational, trusted error: send message to client
-  if (err.isOperatuinal) {
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
-
-    // Programming or other unknown error: don't leak error details
-  } else {
-    // 1) Log error
-    console.error('ERROR', err);
-
-    // 2) Send generic message
-    res.status(500).json({
-      status: 'error',
-      message: 'Something went very wrong!',
+  } //everything that is not marked operational
+  else {
+    console.error('💥 Error! 💥', err);
+    res.status(err.statusCode).json({
+      //status code is always 500
+      status: err.status, //status is always "error"
+      message: "There was an error, it's a problem from the server side! :(",
     });
   }
 };
-
 module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
+  err.statusCode = err.statusCode || 500; //500 because of mongoose or something else. (unknown)
   err.status = err.status || 'error';
-
-  if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+  if (process.env.NODE_ENV === 'development') sendErrorDev(err, res);
+  else if (process.env.NODE_ENV === 'production') {
+    let error = Object.create(err);
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    sendErrorProd(error, res);
+    //sendErrorProd(error, res);
   }
 };
