e473d9ec4708ab2582239f656632ce7b5f70bbb7
RAYNingTime <ivan0kosyakov@gmail.com>
2022-07-26 11:29:13 +0300

    120. Handling Duplicate Database Fields

--- NodeJS_Course/4-natours/starter/controllers/errorController.js
+++ NodeJS_Course/4-natours/starter/controllers/errorController.js
@@ -4,6 +4,13 @@ const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
+
+const handleDublicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -35,6 +42,7 @@ module.exports = (err, req, res, next) => {
   else if (process.env.NODE_ENV === 'production') {
     let error = Object.create(err);
     if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDublicateFieldsDB(err);
     sendErrorProd(error, res);
     //sendErrorProd(error, res);
   }
