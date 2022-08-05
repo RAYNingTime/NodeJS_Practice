ad4115f0b0a512871b601eba1118671ee568d5ce
RAYNingTime <ivan0kosyakov@gmail.com>
2022-08-05 10:59:36 +0300

    145. Data Sanitization

--- NodeJS_Course/4-natours/starter/app.js
+++ NodeJS_Course/4-natours/starter/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -28,6 +31,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL Query injection
+app.use(mongoSanitize());
+
+// Dtata sanitization agains XSS
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
